0664b03dce938a2f77891c99558e89e68fc902ac
Gillian Masija <gillianmasija@gmail.com>
2023-10-30 17:08:09 +0300

    Created the login page

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.querySelector("#login_button button");
+
+    loginButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent form submission for this example
+
+        const username = document.querySelector('input[type="text"]').value;
+        const password = document.querySelector('input[type="password"]').value;
+
+        // Perform simple validation (you can enhance this as needed)
+        if (username === "yourUsername" && password === "yourPassword") {
+            // Successful login
+            alert("Login successful!");
+        } else {
+            // Failed login
+            alert("Invalid username or password. Please try again.");
+        }
+    });
+});
